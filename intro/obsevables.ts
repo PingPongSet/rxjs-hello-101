2c3de5280f00746ec9a2c61614d59eb808550c89
PingPongSet <pingpongofkingkong@gmail.com>
2021-05-16 23:56:52 +0100

    2 files added, 2 deleted

--- intro/obsevables.ts
+++ intro/obsevables.ts
@@ -1,6 +1,11 @@
 import { fromEvent } from 'rxjs';
+import { from, Observable } from 'rxjs/dist/types';
 import { throttleTime, map, scan } from 'rxjs/operators';
 //https://rxjs.dev/guide/Observable
+
+ console.log('done');
+unsubscribe();
+ console.log('done');
 
 //Observables are able to deliver values either synchronously or asynchronously.
 function multiValues() {
@@ -35,4 +40,26 @@ function multiValues() {
   // just after subscribe
   // got value 4
   // done
+}
+
+//It is a good idea to wrap any code in subscribe with try/catch block that will deliver an Error notification if it catches an exception:
+function errorTest() {
+  const observable = new Observable(function subscribe(subscriber) {
+    try {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      subscriber.complete();
+      subscriber.next(4); //never runs
+    } catch (err) {
+      subscriber.error(err); // delivers an error if it caught one
+    }
+  });
+}
+
+function unsubscribe() {
+  const observable = from([10, 20, 30]);
+  const subscription = observable.subscribe(x => console.log(x));
+  // Later:
+  subscription.unsubscribe();
 }
